59104617aadded658fe29726964ef7c9d200530c
ian_b <ian_b@IAN-PC>
2018-03-07 18:43:05 +0000

    minor updates

--- App5/AppPages/account.xaml.cs
+++ App5/AppPages/account.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -23,32 +25,107 @@ namespace App5
     /// </summary>
     public sealed partial class account : Page
     {
+        public class navigationItems
+        {
+            public SymbolIcon headericon { get; set; }
+            public string title { get; set; }
+        }
+
+        private List<navigationItems> listOfSettings = new List<navigationItems>();
+
+
         public account()
         {
             this.InitializeComponent();
+
+            listOfSettings.Add(new navigationItems { title = "Personal Information" });
+            listOfSettings.Add(new navigationItems { title = "Contact Preferences" });
+            listOfSettings.Add(new navigationItems { title = "Security Settings" });
+            listOfSettings.Add(new navigationItems { title = "Privacy Settings" });
+
+            navigationLinks.ItemsSource = listOfSettings;
         }
 
-        public class navigationItems
+
+        private void navigationLinks_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            public string title { get; set; }
-            public Symbol Symbol { get; set; }
+            Debug.WriteLine("Selection Changed");
+
+            switch (navigationLinks.SelectedIndex)
+            {
+                case 0:
+
+                    defaultView.Visibility = Visibility.Collapsed;
+                    defaultViewIcons.Visibility = Visibility.Collapsed;
+
+                    personalInformationPanel.Visibility = Visibility.Visible;
+                    contactInformationPanel.Visibility = Visibility.Collapsed;
+                    securitySettingsPanel.Visibility = Visibility.Collapsed;
+                    privacySettingsPanel.Visibility = Visibility.Collapsed;
+                    break;
+
+                case 1:
+
+                    defaultView.Visibility = Visibility.Collapsed;
+                    defaultViewIcons.Visibility = Visibility.Collapsed;
+
+                    personalInformationPanel.Visibility = Visibility.Collapsed;
+                    contactInformationPanel.Visibility = Visibility.Visible;
+                    securitySettingsPanel.Visibility = Visibility.Collapsed;
+                    privacySettingsPanel.Visibility = Visibility.Collapsed;
+                    break;
+
+                case 2:
+
+                    defaultView.Visibility = Visibility.Collapsed;
+                    defaultViewIcons.Visibility = Visibility.Collapsed;
+
+                    personalInformationPanel.Visibility = Visibility.Collapsed;
+                    contactInformationPanel.Visibility = Visibility.Collapsed;
+                    securitySettingsPanel.Visibility = Visibility.Visible;
+                    privacySettingsPanel.Visibility = Visibility.Collapsed;
+                    break;
+
+                case 3:
+
+                    defaultView.Visibility = Visibility.Collapsed;
+                    defaultViewIcons.Visibility = Visibility.Collapsed;
+
+                    personalInformationPanel.Visibility = Visibility.Collapsed;
+                    contactInformationPanel.Visibility = Visibility.Collapsed;
+                    securitySettingsPanel.Visibility = Visibility.Collapsed;
+                    privacySettingsPanel.Visibility = Visibility.Visible;
+                    break;
+
+                case 4:
+
+                    defaultView.Visibility = Visibility.Collapsed;
+                    defaultViewIcons.Visibility = Visibility.Collapsed;
+
+                    personalInformationPanel.Visibility = Visibility.Collapsed;
+                    contactInformationPanel.Visibility = Visibility.Collapsed;
+                    securitySettingsPanel.Visibility = Visibility.Collapsed;
+                    privacySettingsPanel.Visibility = Visibility.Collapsed;
+                    break;
+
+                default:
+
+                    defaultView.Visibility = Visibility.Collapsed;
+                    defaultViewIcons.Visibility = Visibility.Collapsed;
+
+                    personalInformationPanel.Visibility = Visibility.Collapsed;
+                    contactInformationPanel.Visibility = Visibility.Collapsed;
+                    securitySettingsPanel.Visibility = Visibility.Collapsed;
+                    privacySettingsPanel.Visibility = Visibility.Collapsed;
+                    break;
+            }
+
+
         }
 
-        private ObservableCollection<navigationItems> _navItems = new ObservableCollection<navigationItems>()
+        private void resetPasswordButton_Click(object sender, RoutedEventArgs e)
         {
-            new navigationItems() { title = "navItem1", Symbol=Windows.UI.Xaml.Controls.Symbol.People },
-            new navigationItems() { title = "navItem2", Symbol=Windows.UI.Xaml.Controls.Symbol.Phone },
-        };
-
-        public ObservableCollection<navigationItems> navItems
-        {
-            get { return _navItems; }
-
-        }
-
-        private void navigationLinks_ItemClick(object sender, ItemClickEventArgs e)
-        {
-
+            this.Frame.Navigate(typeof(forgotPasswordPage), new DrillInNavigationTransitionInfo());
         }
     }
 }
